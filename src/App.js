1a448f85e0bea496769e4d5d2cc2e27d373d1ba7
Ameco <hamecow@gmail.com>
2020-11-20 15:26:01 -0800

    Update home nav page

--- src/App.js
+++ src/App.js
@@ -8,11 +8,17 @@ function App() {
   return (
   <header className="Wrapper" >
     <div className="scene-grid"> 
+      <div className="FullStackTitle">
+        <h1>FULL</h1>
+        <h1>STACK</h1>
+        <h1>DEVELOPER</h1>
+      </div>
       <div className="Buttons">
         <AboutButton/>
         <ContactButton/>
         <PortfolioButton/>
       </div>
+      <div className="emptyTwo"></div>
     </div>
   </header>
   );
