4d9e24adb0b9615e11387f54a1e9b76cd9a61465
Ameco <hamecow@gmail.com>
2020-11-20 18:57:51 -0800

    Portfolio Update

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import "./Navigation.css"
  
 import { NavLink } from 'react-router-dom';
  
 const Navigation = () => {
     return (
-       <div>
-          <NavLink to="/">Home</NavLink>
-          <NavLink to="/about">About</NavLink>
-          <NavLink to="/contact">Contact</NavLink>
-          <NavLink to="/portfolio">Portfolio</NavLink>
+       <div className="nav-bar">
+          <NavLink to="/" className="navLink">Home</NavLink>
+          <NavLink to="/about" className="navLink">About</NavLink>
+          <NavLink to="/contact" className="navLink">Contact</NavLink>
+          <NavLink to="/portfolio" className="navLink">Portfolio</NavLink>
        </div>
     );
 }
--- src/components/portfolio/PortfolioDataDisplay.js
+++ src/components/portfolio/PortfolioDataDisplay.js
@@ -3,13 +3,13 @@ import React from 'react'
 export default function PortfolioDataDisplay() {
     return (
         <div className="PortfolioLinks">
-            <a className="link" href="https://github.com/hawenger">Se7en</a>
-            <a className="link" href="https://github.com/hawenger">Robert</a>
-            <a className="link" href="https://github.com/hawenger">Burger Police</a>
-            <a className="link" href="https://github.com/hawenger">EyePass</a>
-            <a className="link" href="https://github.com/hawenger">Clickbait Quiz</a>
-            <a className="link" href="https://github.com/hawenger">Turnip Da Beet</a>
-            <a className="link" href="https://github.com/hawenger">On My Team</a>
+            <a className="link" href="http://se-7-en.herokuapp.com/">Se7en</a>
+            <a className="link" href="https://vast-depths-13570.herokuapp.com/">Robert</a>
+            <a className="link" href="https://burgerpolice.herokuapp.com/">Burger Police</a>
+            <a className="link" href="https://hawenger.github.io/PasswordGenerator/">EyePass</a>
+            <a className="link" href="https://hawenger.github.io/ClickbaitQuiz/">Clickbait Quiz</a>
+            <a className="link" href="https://sbhwang23.github.io/Turnip-Da-Beet/">Turnip Da Beet</a>
+            <a className="link" href="https://warm-river-71685.herokuapp.com/">Note Taker</a>
         </div>
     )
 }
